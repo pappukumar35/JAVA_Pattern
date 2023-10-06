681831b4d0057117ee7db181e0b2465cbe0c94bd
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-06 14:04:43 +0530

    Add files via upload

---
+++ pyradigam.java
@@ -0,0 +1,12 @@
+public class pyradigam {
+    public static void main(String args[]) {
+
+        int n = 6;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
